9386bb863f420e75350aaa2f0d24afa490cdce36
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2024-09-05 12:04:11 -0400

    refactor: only compute center point once

--- R/functions.R
+++ R/functions.R
@@ -93,16 +93,17 @@ hectare_at <- function(lat, lon) {
   
   crs <- 'EPSG:4326'
   center <- c(lat, lon)
+  center_point <- st_point(center)
   # 0.001 degrees is about 111.17 meters at the equator, so start with half of that
   approx_offset <- 0.001 / 2
-  a <- st_sfc(st_point(center) + c( approx_offset, 0), crs = crs)
-  b <- st_sfc(st_point(center) + c(-approx_offset, 0), crs = crs)
+  a <- st_sfc(center_point + c( approx_offset, 0), crs = crs)
+  b <- st_sfc(center_point + c(-approx_offset, 0), crs = crs)
   # The result is the distance between a and b is in meters per 0.001 degree;
   # invert and multiply by 50 to get degrees per 50 meters
   fifty_meter_angle_lat <- 0.05 / as.numeric(st_distance(a, b))
   
-  c <- st_sfc(st_point(center) + c(0,  approx_offset), crs = crs)
-  d <- st_sfc(st_point(center) + c(0, -approx_offset), crs = crs)
+  c <- st_sfc(center_point + c(0,  approx_offset), crs = crs)
+  d <- st_sfc(center_point + c(0, -approx_offset), crs = crs)
   fifty_meter_angle_lon <- 0.05 / as.numeric(st_distance(c, d))
   
   south_west <- c(-fifty_meter_angle_lat, -fifty_meter_angle_lon)
