d2243568f9610f8260c455897d22554c2fa830e9
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2025-01-30 11:05:05 -0500

    refactor: refactor to targets

--- R/nk_functions.R
+++ R/nk_functions.R
@@ -5,6 +5,36 @@ nk_read_table1 <- function(file) {
   )
 }
 
+nk_read_fig2 <- function(dir) {
+  nk_read_fig2_helper <- function(dir, column) {
+    filename <- file.path(dir, paste0("NK_Fig2_", column, ".csv"))
+    read_csv(
+      filename,
+      col_names = c("Year", "value"),
+      # Read columns as double, since that is how they were written
+      col_types = "dd"
+    ) |>
+    # Round columns to nearest integer, since that is what was intended
+    mutate(
+      Year = round(Year),
+      name = column,
+      value = round(value)
+    )
+  }
+  columns <- c(
+    "NoManagement",
+    "ClearcutHigh", "ClearcutLow",
+    "ShelterwoodHigh", "ShelterwoodLow",
+    "ITS_LowHigh", "ITS_LowLow", "ITS_HighHigh", "ITS_HighLow"
+  )
+  bind_rows(lapply(columns, \(column) nk_read_fig2_helper(dir, column) )) |>
+    pivot_wider(id_cols = "Year") |>
+    # The last year in Fig2 is 2164; nudge it to 2165
+    # to align with contemporary FVS runs.
+    mutate(Year = if_else(Year==2164,2165,Year)) |>
+    arrange(Year)
+}
+
 nk_extract_cols_from_plot_code <- function(.data) {
   nk_table1_expanded <- .data |>
     mutate(STATECD = substr(`FIA plot code`, 1, 2)) |>
--- _targets.R
+++ _targets.R
@@ -15,11 +15,16 @@ list(
   tar_target(fiadb, "data/raw/SQLite_FIADB_ENTIRE.db", format = "file"),
   tar_target(nk_table1_csv, "data/raw/NK_Table_1.csv", format = "file"),
 
+  # 01_IdentifyingStands.Rmd
   tar_target(nk_table1, nk_read_table1(nk_table1_csv)),
   tar_target(nk_table1_expanded, nk_extract_cols_from_plot_code(nk_table1)),
   tar_target(nk_all_plot, nk_load_plot_all_invyr(nk_table1_expanded, fiadb)),
   tar_target(nk_matching_plot, nk_match_plots(nk_table1_expanded, nk_all_plot)),
   tar_target(nk_to_fia, nk_transalte_to_fia(nk_matching_plot)),
   tar_target(nk_to_fvs, nk_translate_to_fvs(nk_to_fia, fiadb)),
-  tar_render(identifying_stands, "01_IdentifyingStands.Rmd")
+  # tar_render(identifying_stands, "01_IdentifyingStands.Rmd")
+
+  # 02_NKProjectedCarbon.Rmd
+  tar_target(nk_fig2_dir, "data/raw/nk_fig2/", format = "file"),
+  tar_target(nk_fig2, nk_read_fig2(nk_fig2_dir))
 )
