aa5eb16b8b6f13154cf7667c0c7804aeb3752111
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2025-02-10 17:23:45 -0500

    refactor: use targets markdown

--- R/nk_functions.R
+++ R/nk_functions.R
@@ -5,12 +5,3 @@ nk_extract_regen <- function(nk_regen, mgmt_id) {
     filter(!is.na(DENSITY)) |>
     mutate(STAND_CN = NA, YEAR = NA)
 }
-
-nk_generate_plots_grown <- function(fiadb, nk_matching_plot) {
-  fia_plots_filtered(fiadb, nk_matching_plot, \(.data, con) {
-    .data |>
-      filter_plots_forested(con) |> # Forested implies not skipped
-      filter_plots_undisturbed(con) |>
-      filter_plots_untreated(con)
-  })
-}
