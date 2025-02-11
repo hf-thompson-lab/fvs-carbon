4562dc082dd25c00bd55af0935a1d7a116974ddf
Nik Bates-Haus <nbateshaus@gmail.com>
2025-02-10 19:20:42 -0500

    fix: prefix tarchetypes targets

--- _targets_r/targets/nk_bycond_none_carbon.R
+++ _targets_r/targets/nk_bycond_none_carbon.R
@@ -1,5 +1,5 @@
 # TODO: fvs_run by condition doesn't work b/c stand_cn needs fixed
-tar_file_read(
+tarchetypes::tar_file_read(
   nk_bycond_none_carbon,
   "data/fvs/FVS_NKByCondition_NONE_Carbon.csv",
   read_csv(!!.x, col_types = cols(StandID = col_character()))
