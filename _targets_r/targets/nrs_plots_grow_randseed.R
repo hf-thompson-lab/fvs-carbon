640a044a10922e385ecc97c0d69c81842a73229c
Nik Bates-Haus <nbateshaus@gmail.com>
2025-02-23 22:32:20 -0500

    feat: use large random integers

--- _targets_r/targets/nrs_plots_grow_randseed.R
+++ _targets_r/targets/nrs_plots_grow_randseed.R
@@ -2,5 +2,6 @@ tar_target(nrs_plots_grow_randseed, {
   # We can have as many seeds as we want. We'll convert these to valid FVS
   # random seeds later.
   # Start with 3, so it's reasonably fast but we get some benefit from it.
-  1:3
+  # FVS requires positive, odd-numbered seeds, so multiply by 2 and subtract 1.
+  sample.int(500000, 3) * 2 - 1
 })
