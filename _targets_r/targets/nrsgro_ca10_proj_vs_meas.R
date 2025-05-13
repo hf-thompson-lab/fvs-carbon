1a04a783bd623fa469f62926a8238c9ae942c3b7
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2025-05-13 10:20:39 -0400

    fix: use surveyed data as baseline, not uncalibrated projection

--- _targets_r/targets/nrsgro_ca10_proj_vs_meas.R
+++ _targets_r/targets/nrsgro_ca10_proj_vs_meas.R
@@ -8,7 +8,7 @@ tar_target(nrsgro_ca10_proj_vs_meas, {
     select(STAND_ID, MEASYEAR) |>
     rename(StandID = STAND_ID, StartYear = MEASYEAR)
   
-  none_carbon_tmp <- nrsgro_none_carbon |>
+  srvy_carbon_tmp <- nrsgro_srvy_carbon |>
     rename(Carbon = Aboveground_Total_Live) |>
     select(StandID, Year, Carbon) |>
     group_by(StandID) |>
@@ -16,6 +16,16 @@ tar_target(nrsgro_ca10_proj_vs_meas, {
     mutate(Carbon_Delta = Carbon - lag(Carbon)) |>
     ungroup() |>
     filter(!is.na(Carbon_Delta))
+  
+  srvy_summary_tmp <- nrsgro_srvy_summary |>
+    group_by(StandID) |>
+    arrange(Year) |>
+    mutate(
+      BA_Delta = BA - lag(BA),
+      Tpa_Delta = Tpa - lag(Tpa)
+    ) |>
+    ungroup() |>
+    filter(!is.na(BA_Delta ))
   
   ca10_carbon_tmp <- nrsgro_ca10_carbon |>
     rename(Carbon = Aboveground_Total_Live) |>
@@ -26,16 +36,18 @@ tar_target(nrsgro_ca10_proj_vs_meas, {
     ungroup() |>
     filter(!is.na(Carbon_Delta))
   
-  nrsgro_ca10_proj_vs_meas <- none_carbon_tmp |>
-    rename(Carbon_None = Carbon, Carbon_Delta_None = Carbon_Delta) |>
+  nrsgro_ca10_proj_vs_meas <- srvy_carbon_tmp |>
+    rename(Carbon_Srvy = Carbon, Carbon_Delta_Srvy = Carbon_Delta) |>
     left_join(ca10_carbon_tmp, by = join_by(StandID, Year)) |>
     rename(Carbon_Calb = Carbon, Carbon_Delta_Calb = Carbon_Delta) |>
+    left_join(nrsgro_srvy_summary |> select(StandID, Year, BA), by = join_by(StandID, Year)) |>
+    rename(BA_Srvy = BA) |>
     left_join(stand_mixin, by = join_by(StandID)) |>
     mutate(
-      Carbon_Diff = Carbon_Calb - Carbon_None,
+      Carbon_Diff = Carbon_Calb - Carbon_Srvy,
       ProjectionYears = Year - StartYear,
       Carbon_Flux_Calb = Carbon_Delta_Calb / ProjectionYears,
-      Carbon_Flux_None = Carbon_Delta_None / ProjectionYears,
-      Carbon_Flux_Delta = Carbon_Flux_Calb - Carbon_Flux_None
+      Carbon_Flux_Srvy = Carbon_Delta_Srvy / ProjectionYears,
+      Carbon_Flux_Residual = Carbon_Flux_Calb - Carbon_Flux_Srvy
     )
 })
