048844eaa5e795a6aaa4f80fac6f0db281335be6
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2026-09-13 15:11:14 -0400

    fix: as of FVS 2026.04 FVS_CutList_East is merged into FVS_CutList

--- _targets_r/targets/cficop_hvst_cutlist.R
+++ _targets_r/targets/cficop_hvst_cutlist.R
@@ -1,4 +1,4 @@
 tar_target(cficop_hvst_cutlist, {
-  fvs_read_output(cficop_hvst, "FVS_CutList_East") |>
+  fvs_read_output(cficop_hvst, "FVS_CutList") |>
     filter(random_seed == min(random_seed))
 })
