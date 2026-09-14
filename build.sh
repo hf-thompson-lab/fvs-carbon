12c8bf76640c942bca7388e54fe9c2023d9d4f01
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2026-09-14 10:33:44 -0400

    build: update RStudio build to work on MacOS

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Remove artifacts that no longer align with targets in the pipeline
 R -q -e "targets::tar_prune()"
+
+# Build the artifacts in the pipeline
 R -q -e "targets::tar_make()"
